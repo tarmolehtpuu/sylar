35d395d2e2de980173a3f3de0d495c3d9f1f2e44
Tarmo Lehtpuu <tarmo.lehtpuu@perfectline.ee>
2013-03-27 19:41:15 +0200

    Moved common code from ParserFormula and ParserSequent to Parser super class.

--- src/ee/moo/skynet/input/ParserFormula.java
+++ src/ee/moo/skynet/input/ParserFormula.java
@@ -13,99 +13,28 @@ import java.util.List;
  */
 public class ParserFormula extends Parser {
 
-    private AlphabetFormula alphabet = new AlphabetFormula();
-
     @Override
     public Formula parse(String input) {
 
-        Lexer lexer = new Lexer(input, alphabet);
+        Lexer lexer = new Lexer(input, new AlphabetFormula());
         Token token;
 
         List<Token> tokens = new ArrayList<Token>(input.length());
 
         while ((token = lexer.next()) != null) {
 
-            if (token.getType() != TokenType.WHITESPACE) {
+            if (token.isWhitespace()) {
                 tokens.add(token);
             }
 
         }
 
-        Formula formula = parseExpression(tokens, 0);
+        Formula formula = parseExpression(tokens, 0, Token.Side.NONE);
 
         if (!tokens.isEmpty()) {
             throw new ParserException(String.format("Expecting end of input: %d token(s) remaining", tokens.size()));
         }
 
         return formula;
-    }
-
-    private Formula parseExpression(List<Token> tokens, int precedence) {
-
-        Formula formula = parsePrimary(tokens);
-
-        while (true) {
-
-            if (tokens.isEmpty()) {
-                break;
-            }
-
-            Token token = tokens.get(0);
-
-            if (!token.isBinary()) {
-                break;
-            }
-
-            if (token.getPrecedence() <= precedence) {
-                break;
-            }
-
-            tokens.remove(0);
-
-            if (tokens.isEmpty()) {
-                throw new ParserException("Unexpected end of input");
-            }
-
-            formula = new Formula(token.getNodeType(Token.Side.NONE), formula, parseExpression(tokens, token.getPrecedence()));
-
-
-        }
-
-        return formula;
-    }
-
-    private Formula parsePrimary(List<Token> tokens) {
-
-        if (tokens.isEmpty()) {
-            throw new ParserException(String.format("Unexpected end of input, expecting: %s or %s or %s",
-                    TokenType.STATEMENT, TokenType.LEFTPAREN, TokenType.INVERSION));
-        }
-
-        Token token = tokens.remove(0);
-
-        if (token.isStatement()) {
-            return new Formula(Formula.NodeType.STATEMENT, token.getData());
-
-        } else if (token.isInversion()) {
-            return new Formula(Formula.NodeType.INVERSION, parseExpression(tokens, token.getPrecedence()));
-
-        } else if (token.isLeftParenthesis()) {
-
-            Formula formula = parseExpression(tokens, 0);
-
-            if (tokens.isEmpty()) {
-                throw new ParserException(String.format("Unexpected end of input, expecting: %s", TokenType.RIGHTPAREN));
-            }
-
-            if (!tokens.remove(0).isRightParenthesis()) {
-                throw new ParserException(String.format("Unexpected token: %s, expecting: %s", token, TokenType.RIGHTPAREN));
-            }
-
-            return formula;
-
-        } else {
-            throw new ParserException(String.format("Unexpected token: %s", token));
-        }
-
     }
 }
--- src/ee/moo/skynet/input/ParserSequent.java
+++ src/ee/moo/skynet/input/ParserSequent.java
@@ -13,13 +13,10 @@ import java.util.List;
  */
 public class ParserSequent extends Parser {
 
-
-    private AlphabetSequent alphabet = new AlphabetSequent();
-
     @Override
     public Formula parse(String input) {
 
-        Lexer lexer = new Lexer(input, alphabet);
+        Lexer lexer = new Lexer(input, new AlphabetSequent());
         Token token;
 
         List<Token> left = new ArrayList<Token>();
@@ -84,85 +81,4 @@ public class ParserSequent extends Parser {
 
     }
 
-    private Formula parseExpression(List<Token> tokens, int precedence, Token.Side side) {
-
-        Formula formula = parsePrimary(tokens, side);
-
-        while (true) {
-
-            if (tokens.isEmpty()) {
-                break;
-            }
-
-            Token token = tokens.get(0);
-
-            if (!token.isBinary()) {
-                break;
-            }
-
-            if (token.getPrecedence() <= precedence) {
-                break;
-            }
-
-            tokens.remove(0);
-
-            if (tokens.isEmpty()) {
-                throw new ParserException("Unexpected end of input");
-            }
-
-            formula = new Formula(token.getNodeType(side), formula, parseExpression(tokens, token.getPrecedence(), side));
-
-        }
-
-        return formula;
-    }
-
-    private Formula parsePrimary(List<Token> tokens, Token.Side side) {
-
-        if (tokens.isEmpty()) {
-            throw new ParserException(String.format("Unexpected end of input, expecting: %s or %s or %s",
-                    TokenType.STATEMENT, TokenType.LEFTPAREN, TokenType.INVERSION));
-        }
-
-        Token token = tokens.remove(0);
-
-        if (token.isStatement()) {
-            return new Formula(Formula.NodeType.STATEMENT, token.getData());
-
-        } else if (token.isInversion()) {
-            return new Formula(Formula.NodeType.INVERSION, parseExpression(tokens, token.getPrecedence(), side));
-
-        } else if (token.isLeftParenthesis()) {
-
-            Formula formula = parseExpression(tokens, 0, side);
-
-            if (tokens.isEmpty()) {
-                throw new ParserException(String.format("Unexpected end of input, expecting: %s", TokenType.RIGHTPAREN));
-            }
-
-            if (!tokens.remove(0).isRightParenthesis()) {
-                throw new ParserException(String.format("Unexpected token: %s, expecting: %s", token, TokenType.RIGHTPAREN));
-            }
-
-            return formula;
-
-        } else {
-            throw new ParserException(String.format("Unexpected token: %s", token));
-        }
-
-    }
-
-    public static void main(String[] args) {
-
-
-        String[] formulas = new String[]{
-                "E,C→",
-                "→E,C",
-                "E→C"
-        };
-
-        for (String input : formulas) {
-            System.out.println(String.format("%s = %s", input, Formula.parse(input)));
-        }
-    }
 }
