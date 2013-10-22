4769d1240fd7fe6ef814b41c77815f9cfc24e473
Tarmo Lehtpuu <tarmo.lehtpuu@perfectline.ee>
2013-10-22 15:26:57 +0300

    Revert: Removed unused FormulaException constructors.

--- src/ee/moo/sylar/formula/FormulaException.java
+++ src/ee/moo/sylar/formula/FormulaException.java
@@ -7,7 +7,18 @@ package ee.moo.sylar.formula;
  */
 public class FormulaException extends RuntimeException {
 
+    public FormulaException() {
+    }
+
     public FormulaException(String s) {
         super(s);
+    }
+
+    public FormulaException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public FormulaException(Throwable throwable) {
+        super(throwable);
     }
 }
